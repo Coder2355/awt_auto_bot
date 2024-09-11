8439f077512bed135d0b1194f514c133cc344362
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-11 16:43:02 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+class Config:
+    API_ID = "21740783"  # Your API ID from my.telegram.org
+    API_HASH = "a5dc7fec8302615f5b441ec5e238cd46"  # Your API Hash from my.telegram.org
+    BOT_TOKEN = "7444872585:AAHYzPX_gygFh9xYvu0-k7YOUg7BSG_hzHg"  # Your bot token from BotFather
+
+    # Channel and bot IDs
+    SOURCE_CHANNEL = "-1002183423252"  # Source channel ID where the videos are posted
+    TARGET_CHANNEL = "-1002245327685"  # Target channel ID where the formatted posts will be sent
+    FILE_STORE_BOT = "7286203469"  # File store bot/channel ID where the files are stored
+
+    # Thumbnail file path
+    THUMBNAIL_PATH = "thumbnail.jpg"
