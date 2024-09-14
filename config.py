4cd77af2c60178b758943225f50a5dc950db41f2
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-14 17:05:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "21740783"))  # Your Telegram API ID
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")  # Your Telegram API Hash
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7444872585:AAHYzPX_gygFh9xYvu0-k7YOUg7BSG_hzHg")  # Main bot token
-FILE_STORE_BOT_TOKEN = os.getenv("FILE_STORE_BOT_TOKEN", "7116266807:AAFiuS4MxcubBiHRyzKEDnmYPCRiS0f3aGU")  # File store bot token
 
 # Channel details
 DB_CHANNEL = int(os.getenv("DB_CHANNEL", "-1002234974607"))  # ID of the channel where files are stored
