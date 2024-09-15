5e708a28b2f89a8bfc45e6d73edf6b12e810081b
Coder2355 <168324938+Coder2355@users.noreply.github.com>
2024-09-15 14:51:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "21740783"))  # Your Telegram API ID
 API_HASH = os.getenv("API_HASH", "a5dc7fec8302615f5b441ec5e238cd46")  # Your Telegram API Hash
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7444872585:AAHYzPX_gygFh9xYvu0-k7YOUg7BSG_hzHg")  # Main bot token
-
+ADMINS = int(os.getenv("ADMINS", "6299192020"))
 # Channel details
 DB_CHANNEL = int(os.getenv("DB_CHANNEL", "-1002234974607"))  # ID of the channel where files are stored
 SOURCE_CHANNEL = int(os.getenv("SOURCE_CHANNEL", "-1002183423252"))  # ID of the source channel where videos are sent
